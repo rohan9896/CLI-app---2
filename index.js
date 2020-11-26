4bb1185a256b2631ad38f2ebc9c7da72cd37dedd
Rohan Gupta <56235204+rohan9896@users.noreply.github.com>
2020-11-26 20:11:08 +0530

    refractor: added questions array

--- index.js
+++ index.js
@@ -21,3 +21,47 @@ console.log(chalk.bgYellow("Let's gooooo", nameOfUser))
 console.log();
 
 var score = 0;
+
+var questions = [
+    {
+      question: "Which data structure should be used for fast retrieval of data at a given index?",
+      a: "Tree",
+      b: "Stack",
+      c: "Array",
+      d: "linkedList",
+      answer: "c"
+    },
+    {
+      question: "Which of the following is a non-linear data structure?",
+      a: "LinkedList",
+      b: "Stack",
+      c: "HashMap",
+      d: "Tree",
+      answer: "d"
+    },
+    {
+      question: "Which data structure stores data in a first-in first-out (FIFO) order?",
+      a: "Queue",
+      b: "Stack",
+      c: "Array",
+      d: "Graph",
+      answer: "a"
+    },
+    {
+      question: "Which data structure organizes data in an interconnected network?",
+      a: "Queue",
+      b: "HashMap",
+      c: "Heap",
+      d: "Graph",
+      answer: "d"
+    },
+    {
+      question: "Which data structure takes constant time for lookup of data?",
+      a: "Stack",
+      b: "Heap",
+      c: "HashMap",
+      d: "Graph",
+      answer: "c"
+    },
+  ]
+  
